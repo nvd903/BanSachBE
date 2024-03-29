0af39da7b5d454be474ae58e6b4d8d81a161a92d
dung021102 <dung02112222@gmail.com>
2024-03-29 23:16:47 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const app = express();
+const port = 3001;
+require("dotenv").config();
+
+// const route = require("./routes");
+
+// const db = require("./config/db");
+// db.connect();
+
+const morgan = require("morgan");
+
+app.use(morgan("combined"));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+//router init
+route(app);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
